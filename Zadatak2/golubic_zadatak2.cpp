e45ab5e44e27be39cb68ed87f83693116eab97d5
Fran <lovro5.novak@gmail.com>
2019-02-26 10:27:48 +0100

    Create golubic_zadatak2.cpp

---
+++ Zadatak2/golubic_zadatak2.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	char x;
+	cin >> x;
+	if (x == 'a')
+	{
+		cout << a + b;
+	}
+	if (x == 'b')
+	{
+		cout << max(a, b);
+	}
+	if (x == 'c')
+	{
+		cout << (a + b) / (double)2 << endl;
+		cout << sqrt((a*b));
+	}
+	if (x == 'd')
+	{
+		if (b<a)
+		{
+			swap(a, b);
+		}
+		int y = 0;
+		for (int i = a; i<b; i++)
+		{
+			if (y == 5)
+			{
+				cout << endl;
+				y = 0;
+			}
+			if (i % 2 != 0)
+			{
+				cout << i << " ";
+				y++;
+			}
+		
