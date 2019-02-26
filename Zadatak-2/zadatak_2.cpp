fde5f86b7ffc4787df4641ca0ac53a7c78637c12
KarloGrabant <kgrabant@gmail.com>
2019-02-26 10:13:09 +0100

    Create zadatak_2.cpp

---
+++ Zadatak-2/zadatak_2.cpp
@@ -0,0 +1,88 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double a, b;
+    char z=0;
+    int o;
+    cin>>z;
+    cin>>a>>b;
+    switch (z)
+    {
+        case 'a':{cout<<a+b<<endl; break;}
+        case 'b':
+            {
+                 if(a<b)
+                    {
+                        cout<<b<<endl;
+                    }
+                  else
+                    {
+                        cout<<a<<endl;
+                    }
+                    break;
+            }
+        case 'c':
+            {
+                cout<<(a+b)/2<<endl;
+                cout<< sqrt(a*b)<<endl;
+                break;
+            }
+        case 'd':
+            {
+                if(a<b)
+                {
+                    o==0;
+                    for(int i=a; i<=b; i++)
+                        {
+                            o++;
+                            if(i%2==1)
+                                {
+                                    cout<<i<<" ";
+                                }
+                            else
+                                {
+                                    cout<<"";
+                                }
+                            if(o==5)
+                                {
+                                cout<<endl;
+                                o==0;
+                                }
+                        }
+                }
+                else
+                {
+                    o==0;
+                    for(int i=b; i<=a; i++)
+                        {
+                            o++;
+                            if(i%2==1)
+                                {
+                                    cout<<i<<" ";
+                                }
+                            else
+                                {
+                                    cout<<"";
+                                }
+                            if(o==5)
+                                {
+                                    cout<<endl;
+                                    o==0;
+                                }
+                        }
+                }
+                break;
+            }
+        case 'e':
+            {
+
+            }
+    }
+
+    return 0;
+}
+
+
