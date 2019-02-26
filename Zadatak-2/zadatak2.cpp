30fe417342124d0d2947c509418a0e1c9b427c3b
VinkoBrkic <vinko.brkic777@gmail.com>
2019-02-26 10:47:16 +0100

    Update zadatak2.cpp

--- Zadatak-2/zadatak2.cpp
+++ Zadatak-2/zadatak2.cpp
@@ -12,40 +12,60 @@ int main ()
     {
         case('a'):
         {
-            cout << a+b << endl;
+            cout << a+b << endl;break;
         }
         case('b'):
         {
             if (a>b)
             {
-                cout << a << endl;
+                cout << a << endl;break;
             }else
-                cout << b << endl;
+                cout << b << endl;break;
         }
         case('c'):
         {
             cout << (a*b)/2 << endl;
-            cout << sqrt(a*b) << endl;
+            cout << sqrt(a*b) << endl;break;
         }
         case ('d'):
         {
             for (int i=a;i<=b;i++)
             {
+                if (i%2!=0)
+                {
+                    y=0;
+                    cout<< i <<" ";
+                    y++;
+
                 if(y==5)
                 {
                     cout << endl;
                     y=0;
                 }
-                if (i%2!=0)
-                {
-                    cout<< i <<" ";
-                    y++;
-                }
+
             }
         }
         case('e'):
         {
-
+            int p1=a, p2=b;
+            while(true)
+            {
+                if (p1==p2)
+                {
+                    cout << p1<< endl;
+                    return 0;
+                }
+                if (p1<p2)
+                {
+                    p1+=a;
+                }else
+                {
+                    p2+=b;
+                }
+            }
         }
+
     }
 }
+}
+
