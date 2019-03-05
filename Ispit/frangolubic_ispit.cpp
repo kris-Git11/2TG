ec8173f05eab26c9dcbf7b3289044f82f783fe29
Fran <lovro5.novak@gmail.com>
2019-03-05 09:50:51 +0100

    Create frangolubic_ispit.cpp

---
+++ Ispit/frangolubic_ispit.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//Zadatak 1
+int main()
+{
+	
+	int x;
+	cin >> x;
+	brojac = x / 3
+	if (x & 100 == 0 x >= 10)
+
+	{
+		if (x % 10 != (x / 10) % 10)
+		{
+			if (x / 3 == 0)
+			{
+				cout << i << "";
+				x++;
+			}
+			else cout << endl;
+		
+				
+		}
+
+	}
+	return 0;
+}
+
+
